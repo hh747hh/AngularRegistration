2a5f70c0256db47b305bafc348077e1a47462547
Woo Yun <hh747hh@gmail.com>
2016-06-19 22:16:41 -0400

    added mvc structure

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config('routerProvider', function($routerProvider) {
+  $routerProvider.
+  when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'RegistrationController'
+  }).
+  when('/register', {
+    templateUrl: 'views/register.html',
+    controller: 'RegistrationController'
+  }).
+  when('/success', {
+    templateUrl: 'view/success.html',
+    controller: 'SuccessController'
+  }).
+  otherwise({
+    redirectTo: '/login'
+  });
+}]);
